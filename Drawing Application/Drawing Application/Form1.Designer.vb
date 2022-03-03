e03727be72015871d0c87430c894eba6038efd4e
Nathaniel Bailey <Nathaniel.bailey419@gmail.com>
2022-03-03 13:45:59 -0700

    First Version

---
+++ Drawing Application/Drawing Application/Form1.Designer.vb
@@ -0,0 +1,307 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Red_1 = New System.Windows.Forms.Button()
+        Me.Blue_1 = New System.Windows.Forms.Button()
+        Me.Green_1 = New System.Windows.Forms.Button()
+        Me.Orange_1 = New System.Windows.Forms.Button()
+        Me.Purple_2 = New System.Windows.Forms.Button()
+        Me.Yellow_1 = New System.Windows.Forms.Button()
+        Me.Yellow_2 = New System.Windows.Forms.Button()
+        Me.Purple_1 = New System.Windows.Forms.Button()
+        Me.Orange_2 = New System.Windows.Forms.Button()
+        Me.Green_2 = New System.Windows.Forms.Button()
+        Me.Blue_2 = New System.Windows.Forms.Button()
+        Me.Red_2 = New System.Windows.Forms.Button()
+        Me.Gray_1 = New System.Windows.Forms.Button()
+        Me.Black = New System.Windows.Forms.Button()
+        Me.Gray_4 = New System.Windows.Forms.Button()
+        Me.Gray_3 = New System.Windows.Forms.Button()
+        Me.Gray_2 = New System.Windows.Forms.Button()
+        Me.White = New System.Windows.Forms.Button()
+        Me.Selected_Color = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Red_1
+        '
+        Me.Red_1.BackColor = System.Drawing.Color.Red
+        Me.Red_1.Location = New System.Drawing.Point(75, 17)
+        Me.Red_1.Name = "Red_1"
+        Me.Red_1.Size = New System.Drawing.Size(23, 23)
+        Me.Red_1.TabIndex = 0
+        Me.Red_1.UseVisualStyleBackColor = False
+        '
+        'Blue_1
+        '
+        Me.Blue_1.BackColor = System.Drawing.Color.Blue
+        Me.Blue_1.Location = New System.Drawing.Point(104, 17)
+        Me.Blue_1.Name = "Blue_1"
+        Me.Blue_1.Size = New System.Drawing.Size(23, 23)
+        Me.Blue_1.TabIndex = 1
+        Me.Blue_1.UseVisualStyleBackColor = False
+        '
+        'Green_1
+        '
+        Me.Green_1.BackColor = System.Drawing.Color.Lime
+        Me.Green_1.Location = New System.Drawing.Point(133, 17)
+        Me.Green_1.Name = "Green_1"
+        Me.Green_1.Size = New System.Drawing.Size(23, 23)
+        Me.Green_1.TabIndex = 2
+        Me.Green_1.UseVisualStyleBackColor = False
+        '
+        'Orange_1
+        '
+        Me.Orange_1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Orange_1.Location = New System.Drawing.Point(162, 17)
+        Me.Orange_1.Name = "Orange_1"
+        Me.Orange_1.Size = New System.Drawing.Size(23, 23)
+        Me.Orange_1.TabIndex = 3
+        Me.Orange_1.UseVisualStyleBackColor = False
+        '
+        'Purple_2
+        '
+        Me.Purple_2.BackColor = System.Drawing.Color.Purple
+        Me.Purple_2.Location = New System.Drawing.Point(191, 46)
+        Me.Purple_2.Name = "Purple_2"
+        Me.Purple_2.Size = New System.Drawing.Size(23, 23)
+        Me.Purple_2.TabIndex = 4
+        Me.Purple_2.UseVisualStyleBackColor = False
+        '
+        'Yellow_1
+        '
+        Me.Yellow_1.BackColor = System.Drawing.Color.Yellow
+        Me.Yellow_1.Location = New System.Drawing.Point(220, 17)
+        Me.Yellow_1.Name = "Yellow_1"
+        Me.Yellow_1.Size = New System.Drawing.Size(23, 23)
+        Me.Yellow_1.TabIndex = 5
+        Me.Yellow_1.UseVisualStyleBackColor = False
+        '
+        'Yellow_2
+        '
+        Me.Yellow_2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Yellow_2.Location = New System.Drawing.Point(220, 46)
+        Me.Yellow_2.Name = "Yellow_2"
+        Me.Yellow_2.Size = New System.Drawing.Size(23, 23)
+        Me.Yellow_2.TabIndex = 12
+        Me.Yellow_2.UseVisualStyleBackColor = False
+        '
+        'Purple_1
+        '
+        Me.Purple_1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Purple_1.Location = New System.Drawing.Point(191, 17)
+        Me.Purple_1.Name = "Purple_1"
+        Me.Purple_1.Size = New System.Drawing.Size(23, 23)
+        Me.Purple_1.TabIndex = 11
+        Me.Purple_1.UseVisualStyleBackColor = False
+        '
+        'Orange_2
+        '
+        Me.Orange_2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Orange_2.Location = New System.Drawing.Point(162, 46)
+        Me.Orange_2.Name = "Orange_2"
+        Me.Orange_2.Size = New System.Drawing.Size(23, 23)
+        Me.Orange_2.TabIndex = 10
+        Me.Orange_2.UseVisualStyleBackColor = False
+        '
+        'Green_2
+        '
+        Me.Green_2.BackColor = System.Drawing.Color.Green
+        Me.Green_2.Location = New System.Drawing.Point(133, 46)
+        Me.Green_2.Name = "Green_2"
+        Me.Green_2.Size = New System.Drawing.Size(23, 23)
+        Me.Green_2.TabIndex = 9
+        Me.Green_2.UseVisualStyleBackColor = False
+        '
+        'Blue_2
+        '
+        Me.Blue_2.BackColor = System.Drawing.Color.Navy
+        Me.Blue_2.Location = New System.Drawing.Point(104, 46)
+        Me.Blue_2.Name = "Blue_2"
+        Me.Blue_2.Size = New System.Drawing.Size(23, 23)
+        Me.Blue_2.TabIndex = 8
+        Me.Blue_2.UseVisualStyleBackColor = False
+        '
+        'Red_2
+        '
+        Me.Red_2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Red_2.Location = New System.Drawing.Point(75, 46)
+        Me.Red_2.Name = "Red_2"
+        Me.Red_2.Size = New System.Drawing.Size(23, 23)
+        Me.Red_2.TabIndex = 7
+        Me.Red_2.Text = " "
+        Me.Red_2.UseVisualStyleBackColor = False
+        '
+        'Gray_1
+        '
+        Me.Gray_1.BackColor = System.Drawing.Color.FromArgb(CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(224, Byte), Integer))
+        Me.Gray_1.Location = New System.Drawing.Point(104, 75)
+        Me.Gray_1.Name = "Gray_1"
+        Me.Gray_1.Size = New System.Drawing.Size(23, 23)
+        Me.Gray_1.TabIndex = 19
+        Me.Gray_1.UseVisualStyleBackColor = False
+        '
+        'Black
+        '
+        Me.Black.BackColor = System.Drawing.Color.Black
+        Me.Black.Location = New System.Drawing.Point(220, 75)
+        Me.Black.Name = "Black"
+        Me.Black.Size = New System.Drawing.Size(23, 23)
+        Me.Black.TabIndex = 18
+        Me.Black.UseVisualStyleBackColor = False
+        '
+        'Gray_4
+        '
+        Me.Gray_4.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.Gray_4.Location = New System.Drawing.Point(191, 75)
+        Me.Gray_4.Name = "Gray_4"
+        Me.Gray_4.Size = New System.Drawing.Size(23, 23)
+        Me.Gray_4.TabIndex = 17
+        Me.Gray_4.UseVisualStyleBackColor = False
+        '
+        'Gray_3
+        '
+        Me.Gray_3.BackColor = System.Drawing.Color.Gray
+        Me.Gray_3.Location = New System.Drawing.Point(162, 75)
+        Me.Gray_3.Name = "Gray_3"
+        Me.Gray_3.Size = New System.Drawing.Size(23, 23)
+        Me.Gray_3.TabIndex = 16
+        Me.Gray_3.UseVisualStyleBackColor = False
+        '
+        'Gray_2
+        '
+        Me.Gray_2.BackColor = System.Drawing.Color.Silver
+        Me.Gray_2.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Gray_2.Location = New System.Drawing.Point(133, 75)
+        Me.Gray_2.Name = "Gray_2"
+        Me.Gray_2.Size = New System.Drawing.Size(23, 23)
+        Me.Gray_2.TabIndex = 15
+        Me.Gray_2.UseVisualStyleBackColor = False
+        '
+        'White
+        '
+        Me.White.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.White.Location = New System.Drawing.Point(75, 75)
+        Me.White.Name = "White"
+        Me.White.Size = New System.Drawing.Size(23, 23)
+        Me.White.TabIndex = 14
+        Me.White.UseVisualStyleBackColor = False
+        '
+        'Selected_Color
+        '
+        Me.Selected_Color.BackColor = System.Drawing.SystemColors.Control
+        Me.Selected_Color.Location = New System.Drawing.Point(15, 36)
+        Me.Selected_Color.Name = "Selected_Color"
+        Me.Selected_Color.Size = New System.Drawing.Size(45, 42)
+        Me.Selected_Color.TabIndex = 20
+        Me.Selected_Color.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(3, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(70, 13)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "Slected Color"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Orange_2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Red_1)
+        Me.GroupBox1.Controls.Add(Me.Selected_Color)
+        Me.GroupBox1.Controls.Add(Me.Blue_1)
+        Me.GroupBox1.Controls.Add(Me.Gray_1)
+        Me.GroupBox1.Controls.Add(Me.Green_1)
+        Me.GroupBox1.Controls.Add(Me.Black)
+        Me.GroupBox1.Controls.Add(Me.Orange_1)
+        Me.GroupBox1.Controls.Add(Me.Gray_4)
+        Me.GroupBox1.Controls.Add(Me.Purple_2)
+        Me.GroupBox1.Controls.Add(Me.Gray_3)
+        Me.GroupBox1.Controls.Add(Me.Yellow_1)
+        Me.GroupBox1.Controls.Add(Me.Gray_2)
+        Me.GroupBox1.Controls.Add(Me.Red_2)
+        Me.GroupBox1.Controls.Add(Me.White)
+        Me.GroupBox1.Controls.Add(Me.Blue_2)
+        Me.GroupBox1.Controls.Add(Me.Yellow_2)
+        Me.GroupBox1.Controls.Add(Me.Green_2)
+        Me.GroupBox1.Controls.Add(Me.Purple_1)
+        Me.GroupBox1.Location = New System.Drawing.Point(667, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(260, 100)
+        Me.GroupBox1.TabIndex = 22
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Color"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(571, 48)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(54, 51)
+        Me.Button1.TabIndex = 23
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(939, 578)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Red_1 As Button
+    Friend WithEvents Blue_1 As Button
+    Friend WithEvents Green_1 As Button
+    Friend WithEvents Orange_1 As Button
+    Friend WithEvents Purple_2 As Button
+    Friend WithEvents Yellow_1 As Button
+    Friend WithEvents Yellow_2 As Button
+    Friend WithEvents Purple_1 As Button
+    Friend WithEvents Orange_2 As Button
+    Friend WithEvents Green_2 As Button
+    Friend WithEvents Blue_2 As Button
+    Friend WithEvents Red_2 As Button
+    Friend WithEvents Gray_1 As Button
+    Friend WithEvents Black As Button
+    Friend WithEvents Gray_4 As Button
+    Friend WithEvents Gray_3 As Button
+    Friend WithEvents Gray_2 As Button
+    Friend WithEvents White As Button
+    Friend WithEvents Selected_Color As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+End Class
